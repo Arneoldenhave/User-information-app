d45d189063a1e1b4411ec49ea06e39c5718a5e9c
Arne Oldenhave <arneoldenhave@gmail.com>
2017-02-02 14:53:47 +0100

    Commit, 2e pagina, data parsed, mooie dingen

---
+++ Documentation.js
@@ -0,0 +1,14 @@
+res.render(view [,locals] [,callback])
+
+res.render(filepath)
+
+res.render(filepath, callback)
+
+res.render(filepath, locals)
+
+res.render(filepath, locals, callback)
+
+//app post: Routes HTTP POST requests to the specifed path with the specified callback functions.
+
+app.post(path, callback [,callback..])
+
--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const fs = require('fs');
 const bodyParser = require('body-parser')
 const app = express();
 
+
+// app.use(bodyParser)
 const urlencodedParsed = bodyParser.urlencoded ({extended : true})
+
+// app.use(bodyPArser.urlencoded({
+// 	extended: false;
+// }));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
+
+
+
+
+//Frist page with list of users
 app.get('/', (req, res)=> {
 	fs.readFile('./users.json', 'utf8', (err, data) => {
 		if (err) {
@@ -21,16 +32,44 @@ app.get('/', (req, res)=> {
 });
 
 
-// Search page
+//Second page with search bar
 app.get('/search', (req, res) => {
 	res.render ('searchBar')
+	console.log('/search refreshed')
+	
+});
 
-app.post('/search')
+//Name comparerer
+app.post('/search', urlencodedParsed, (req, res) =>{
+	var name=req.body.name;
+	var htmlData = 'Hello: ' + name;
+		fs.readFile('./users.json', 'utf8', (err, data) => {
+			if (err) {
+			throw err;
+			}
+			const users = JSON.parse(data);
+			console.log('data is geparsed vriend')
 
+
+			for (var i = 0; i < users.length; i++) {
+				console.log('Looping')
+				// if (firstName === req.body.firstName[i] && lastName === req.body.lastName[i]) {
+				// console.log('Yeahbuddy!')
+				// }
+			}
+		
+	})
+
+	res.send(htmlData)
+	console.log(htmlData)
+
+	
 })
 
 
 
+
+//Port selector
 app.listen(3000, () => {
 	console.log("Ready, set, GO!")
-});
+})
